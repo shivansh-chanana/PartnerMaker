facfe83af45ae50a2320be92cdc01098d4a7c61c
Shivu MacBook <shivansh.chanana@gmail.com>
2020-03-30 01:15:13 +0530

    Update_1_3
    Background
    Animations

--- Partner Maker/Assets/GameController.cs
+++ Partner Maker/Assets/GameController.cs
@@ -22,6 +22,8 @@ public class GameController : MonoBehaviour
     public Vector2 modelRandomLimit;
     public bool matchCustomerHeadWithBody;
     public bool MTCMatchHeadWithBody;
+    public SpriteRenderer MTC_bg;
+    public Sprite MTC_FemaleBg , MTC_MaleBg;
 
     [Header("UI Variables")]
     [Space]
@@ -32,23 +34,23 @@ public class GameController : MonoBehaviour
     [Space]
     [SerializeField] float armThickness;
     [SerializeField] float legThickness , waistThickness;
-
+    [SerializeField] Texture customerTexture , MTC_Texture;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ResetLevel();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Arm thick function
-        ArmThicknessFunction();
-        //Leg thick function
-        LegThicknessFunction();
-        //Leg thick function
-        WaiseThicknessFunction();
+        ////Arm thick function
+        //ArmThicknessFunction();
+        ////Leg thick function
+        //LegThicknessFunction();
+        ////Leg thick function
+        //WaiseThicknessFunction();
     }
 
     void ArmThicknessFunction() {
@@ -73,9 +75,9 @@ public class GameController : MonoBehaviour
         int randomGender = 0;//Random.Range(0,2);
         //Female gender
         if(randomGender == 0)
-            ResetModel(femaleSkinnedMeshRend , femaleTextures , femaleBodyMat , true);
+            ResetModel(femaleSkinnedMeshRend , femaleTextures , femaleBodyMat , true , true , true);
         else //Male gender
-            ResetModel(maleSkinnedMeshRend, maleTextures, maleBodyMat, true);
+            ResetModel(maleSkinnedMeshRend, maleTextures, maleBodyMat, true , true , false);
 
 
         //Model to create
@@ -83,24 +85,30 @@ public class GameController : MonoBehaviour
         //Female gender
         if (randomGender == 0)
         {
-            ResetModel(Female_MTCSkinRend, femaleTextures, MTCMat, true);
+            ResetModel(Female_MTCSkinRend, femaleTextures, MTCMat, true , false , true);
             female_MTC.SetActive(true);
             male_MTC.SetActive(false);
         }
         else
         {//male gender
-            ResetModel(Male_MTCSkinRend, maleTextures, MTCMat, true);
+            ResetModel(Male_MTCSkinRend, maleTextures, MTCMat, true , false , false);
             female_MTC.SetActive(false);
             male_MTC.SetActive(true);
         }
     }
 
-    void ResetModel(SkinnedMeshRenderer skinnedMeshRenderer , Texture[] modelTextures , Material[] modelMaterials , bool isMatchHead) {
+    void ResetModel(SkinnedMeshRenderer skinnedMeshRenderer , Texture[] modelTextures , Material[] modelMaterials , bool isMatchHead , bool isCustomer , bool isFemale) {
         skinnedMeshRenderer.SetBlendShapeWeight(0,Random.Range(modelRandomLimit.x , modelRandomLimit.y));
         skinnedMeshRenderer.SetBlendShapeWeight(1, Random.Range(modelRandomLimit.x, modelRandomLimit.y));
         skinnedMeshRenderer.SetBlendShapeWeight(2, Random.Range(modelRandomLimit.x, modelRandomLimit.y));
 
+        //Initilization
         Texture bodyTexture = modelTextures[Random.Range(0, modelTextures.Length - 1)];
+        //Never same texture
+        do {
+            bodyTexture = modelTextures[Random.Range(0, modelTextures.Length - 1)];
+        } while (bodyTexture == customerTexture || bodyTexture == MTC_Texture);
+
         modelMaterials[1].mainTexture = bodyTexture;
 
         if (isMatchHead)
@@ -115,6 +123,19 @@ public class GameController : MonoBehaviour
             } while (headTexture == bodyTexture);
 
             modelMaterials[0].mainTexture = headTexture;
+        }
+
+        if (isCustomer) {
+            customerTexture = modelMaterials[1].mainTexture;
+        }
+        else {
+            MTC_Texture = modelMaterials[1].mainTexture;
+        }
+
+        //If this isn't customer
+        if (!isCustomer) {
+            if (isFemale) MTC_bg.sprite = MTC_FemaleBg;
+            else MTC_bg.sprite = MTC_MaleBg;
         }
     }
 }
